e5f15eb6dbabb612e3bd9ed2eef7c4b169fc761d
Gabriel Richard de Oliveira <grichard.oliveira@outlook.com>
2021-05-19 23:08:49 -0300

    ex053 finalizado

---
+++ ex053.py
@@ -0,0 +1,14 @@
+frase = str(input('Digite uma frase: ')).strip()
+# fc = len(frase) - frase.count(' ')
+fs = frase.replace(' ','')
+cont = 0
+for c in range (0, len(fs)):
+    if fs [c] == fs [-c-1]:
+        cont += 1
+
+if cont == fs:
+    print('O inverso de {} é {}'.format(frase, frase[::-1]))
+    print('Temos um PALINDROMO')
+else:
+    print('O inverso de {} é {}'.format(frase, frase[::-1]))
+    print('A frase digitada não é um PALINDROMO')
