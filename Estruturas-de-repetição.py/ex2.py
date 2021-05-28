00e6925c21b71f30a4811f90e37a862cf95dd97d
Gabriel Richard de Oliveira <grichard.oliveira@outlook.com>
2021-05-28 00:28:48 -0300

    finalizado
    Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.

--- Estruturas-de-repetição.py/ex2.py
+++ Estruturas-de-repetição.py/ex2.py
@@ -4,7 +4,6 @@ senha = str(input('Digite sua senha: '))
 while nome == senha:
     print(' '*25,'''\033[31mERRO\033[m
 \033[7;30mNão é permitido que o nome do usuario e senha sejam iguais.\033[m\n''')
-
     nome = str(input('Digite o nome do usuario novamente: '))
     senha = str(input('Digite a senha novamente: '))
 
