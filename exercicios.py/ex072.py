10bf928195b0f1ef165c8856b0e2fc0f1283ded4
Gabriel Richard de Oliveira <grichard.oliveira@outlook.com>
2021-07-06 22:26:25 -0300

    finalizado

--- exercicios.py/ex072.py
+++ exercicios.py/ex072.py
@@ -1,7 +1,10 @@
 listanum = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quartoze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 while True: 
     num = int(input('Digite um numero entre 0 e 20: '))
-    if num >= 0 and num <= 20:
-        break
+    if 0 <= num <=20:
+        print(f'Você digitou o numero {listanum[num]}')
+        resposta = str(input('\nQuer continuar? [S/N]: ')).strip().upper()
+        if resposta == 'N':
+            break       
     print('tente novamente.', end=' ')
-print(f'Você digitou o numero {listanum[num]}')
+
