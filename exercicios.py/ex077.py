933fc02bc098bb52c70aa01a6782ab893c0c1b17
Gabriel Richard de Oliveira <grichard.oliveira@outlook.com>
2021-07-15 23:17:27 -0300

    meio

---
+++ exercicios.py/ex077.py
@@ -0,0 +1,23 @@
+palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSOR', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO',
+'PROGRAMADOR', 'FUTURO')
+
+
+
+
+palavras[0]
+
+
+
+
+
+
+
+
+
+
+for c in palavras:
+    
+    for b in palavras[b].count('A,E,I,O,U'):
+        print
+    
+    print(f'Na palavra {c} temos ')      
