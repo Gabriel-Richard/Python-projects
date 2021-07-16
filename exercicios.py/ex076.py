eafef060cce74ee56cf9e4b3036dce02ad28d495
Gabriel Richard de Oliveira <grichard.oliveira@outlook.com>
2021-07-15 22:30:05 -0300

    f

---
+++ exercicios.py/ex076.py
@@ -0,0 +1,11 @@
+print('-'*50)
+print(f'{"LISTAGEM DE PREÇOS":^50}')
+print('-'*50)
+
+itens = (f'{"Lapis":.<40}R$:{" 1.75":>7}', f'{"Borracha":.<40}R$:{" 2.00":>7}', f'{"Caderno":.<40}R$:{" 15.90":>7}',f'{"Estojo":.<40}R$:{" 25.00":>7}',
+ f'{Tranferidor} R$: 4.20','Compasso  R$: 4.99', 'Mochila R$: 120.32', 'Cananetas R$: 22.30', 'Livro R$: 34.90')
+
+for c in itens:
+    print(c)
+
+print('-'*50)
