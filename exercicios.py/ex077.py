2a4ee01ad121ededab8a1b55a2e28496359d2e9a
Gabriel Richard de Oliveira <grichard.oliveira@outlook.com>
2021-07-16 00:54:59 -0300

    finalizado

--- exercicios.py/ex077.py
+++ exercicios.py/ex077.py
@@ -1,28 +1,21 @@
 palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSOR', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO',
 'PROGRAMADOR', 'FUTURO')
 
-
-
-
-
-
-
 for c in palavras:
-
+    print(f'\nNa palavra {c} temos',end='')
     
-    print(c.count('A'))
-
-    if 'A' in c.len():
-        print ('A')
-
-
-
-
+    if 'A'or 'E' or 'I' or 'O' or 'U' in c:
+        print(c.count('A')* ' A ',end='')
+        print(c.count('E')* ' E ',end='')
+        print(c.count('I')* ' I ',end='')
+        print(c.count('O')* ' O ',end='')
+        print(c.count('U')* ' U ',end='')       
 
 
 
-    # print(c.find('A'))
-    
 
-    
-    #print(f'Na palavra {c} temos ')      
+#for p in palavras:
+#   print(f'\nNa palavra {p} temos ', end='')
+#   for letra in p:
+#   if letra.lower() in 'aeiou':
+#       print (letra, end=' ')
