bd4111ccb0a1c23f228b4e4e2d6699ea86d5352a
Gabriel Richard de Oliveira <grichard.oliveira@outlook.com>
2021-08-03 00:17:09 -0300

    finalizado

--- exercicios.py/ex078.py
+++ exercicios.py/ex078.py
@@ -1,24 +1,20 @@
 lista = []
 
-
 for c in range (0, 5):
     n1 = int(input(f'Digite um valor para a posição {c}: '))
      
     lista.append(n1)    
 
-#for pric in lista [::-1]:
- #   print (c)
-  #  print (lista.index (max(c)))
+for x in range (0, 5):
+    if lista [x] == max(lista):
+       n2 = x
 
-Vm = max(lista)
-
-for x in lista:
-    if x == Vm:
-        num = x
-    print (x)
+for z in range (0, 5):
+    if lista [z] == min(lista):
+        n3 = z
 
 print ('-='*20)
 print (f'Voce digitou os valores {lista}')
-print (f'O maior valor digitado foi {max(lista)}. Nas posições {lista.index(max(lista))}... ...')
-print (f'O menor valor digitado foi {min(lista)}. Nas posições {lista.index(min(lista))}...')
+print (f'O maior valor digitado foi {max(lista)}. Nas posições {lista.index(max(lista))}... {n2}...')
+print (f'O menor valor digitado foi {min(lista)}. Nas posições {lista.index(min(lista))}... {n3}...')
 
